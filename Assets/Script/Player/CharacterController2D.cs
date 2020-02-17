9f2d08312c6b2984846fd09da9e6d5908c95b01c
Giova <giovanni.aravena.13@sansano.usm.cl>
2020-02-17 05:47:15 -0300

    gitflow-feature-stash: parallax

--- Assets/Script/Player/CharacterController2D.cs
+++ Assets/Script/Player/CharacterController2D.cs
@@ -108,6 +108,7 @@ public class CharacterController2D : MonoBehaviour
 			if (Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround))
 			{
 				crouch = true;
+                Debug.Log("Hay un objeto que está tocando el techo del puma");
 			}
 		}
 
