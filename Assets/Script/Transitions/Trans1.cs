452d08e4afb20c31c20fb45f040979cb0623f217
Nacho <sebax-badboy@hotmail.com>
2020-03-26 00:59:05 -0300

    Mapa Terminado + GG

---
+++ Assets/Script/Transitions/Trans1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Trans1 : MonoBehaviour{
+
+    public string newScene;
+    bool enter = false;
+    void OnTriggerEnter2D(Collider2D collision){
+        if (collision.gameObject.name == "Player"){
+            enter = true;
+        }
+    }
+    void OnTriggerStay2D(Collider2D other)
+    {
+        enter = true;
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        enter = false;
+    }
+    void Update(){
+        if ((Input.GetButtonDown("Fire1")) && enter)
+        {
+            SceneManager.LoadScene(newScene, LoadSceneMode.Single);
+        }
+        
+    }
+}
---
+++ Assets/Script/Transitions/TransF.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransF : MonoBehaviour
+{
+
+    public string newScene;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            SceneManager.LoadScene(newScene, LoadSceneMode.Single);
+        }
+    }
+}
